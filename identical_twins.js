0910c885075695e18ae9bf92f2a53250f4adc4f9
Aatikahakim <Aatikahakim@gmail.com>
2023-03-18 16:36:41 +0500

    finding twins by a function

--- identical_twins.js
+++ identical_twins.js
@@ -1,15 +1,15 @@
 // Identical Twins
 
 const checkTwins = (arr) => {
-    let res = [];
-    for(let i=0; i<arr.length; i++){
-        for(let j=i; j<arr.length; j++){
-            if(arr[i] + arr[j] > 0){
-                arr.sort();
-                res.push([arr[i],arr[j]]);
+    let SortedArray = arr.slice().sort(); 
+    let res = []; // store all results
+
+    // traverse through all indexes
+    for(let i=0; i<SortedArray.length; i++){
+            if(SortedArray[i+1] === SortedArray[i]){
+                res.push(SortedArray[i]);
             }
         }
-    }
     return res;
 }
-console.log(checkIdenticalTwin([3,-4,5,-6,4,0,2,-1,0]));
+console.log(checkTwins([3,3,4,4,4,2,7,5,7,4,7,2,-1,0]));
