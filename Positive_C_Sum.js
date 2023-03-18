f193553f0b0f73ca9e99f8c000976e20b718a1f2
Aatikahakim <Aatikahakim@gmail.com>
2023-03-18 16:37:23 +0500

    sum of positive numbers with prefix sum

--- Positive_C_Sum.js
+++ Positive_C_Sum.js
@@ -1,16 +1,15 @@
 // Positive Comulative Sum
 
-const positiveSum = (arr) =>{
-    let res = []; // store all the results
-    let sum = 0;
-
+const positiveComulativeSum = (arr) =>{
+    let res = [];
     for(let i=0; i<arr.length; i++){
         for(let j=i; j<arr.length; j++){
-            if(arr[i] + arr[j] === sum){
-                res.push(arr[i],arr[j]);
+            if(arr[i] + arr[j] > 0){
+                res.push([arr[i],arr[j]]);
             }
         }
     }
     return res;
 }
-console.log(positiveSum([1,3,-4,-5,4,2]));
+console.log(positiveComulativeSum([1,3,-4,-5,4,2]));
+
