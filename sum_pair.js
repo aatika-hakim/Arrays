cc99143014acbded6075408fc09a585e298edb0a
Aatikahakim <Aatikahakim@gmail.com>
2023-03-19 12:19:16 +0500

    sum pairs

--- sum_pair.js
+++ sum_pair.js
@@ -1,12 +1,18 @@
 //  find sum pairs in an array
 
-const squaredArray = (arr) =>{
-    let res = [];
+const sumPair = (arr, sum) =>{
+    let res = []; //store all the results
+
+    // Traverse through an array
     for(let i=0; i<arr.length; i++){
         for(let j=i; j<arr.length; j++){
-            res.push(arr[i], arr[j]);
+            if(arr[i]+ arr[j] === sum)
+            res.push([arr[i], arr[j]]);
         }
     }   
     return res;
 }
-console.log(sumPair([2,4,3,4,8,76,24,1,5]));
+let arr = [2,45,6,0,-12,4,6];
+let sum = 8;
+
+console.log(sumPair(arr, sum));
