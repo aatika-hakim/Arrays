2a7eaa3c321585b26af9615225aad8a8d2d941f9
Aatikahakim <Aatikahakim@gmail.com>
2023-03-23 11:45:56 +0500

    find numbers with even digits

--- Even_digit.js
+++ Even_digit.js
@@ -1,10 +1,17 @@
 //Print Even digits
 
 const evenDigitCount = (arr) =>{  
+    let count = 0;
 
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]%2 == 0)
-        console.log(arr[i]);
-    }  
-}
-console.log(evenDigitCount([2,4,3,433333,8789,76,24,1,5]));
+    for(let num of arr){
+      	// convert to string it is easy to find length
+            num = num.toString();
+		if (num.length % 2 === 0) {
+			count++;
+        }
+    }
+    return count;
+}                                                                                                                                                    
+let arr = [2,45,678,2000,4567];
+
+console.log(evenDigitCount(arr));
